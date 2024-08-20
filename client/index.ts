e6397cc05c35e7482af65e713cc947232712f9ba
Julien Midedji <Julien.Midedji@gmail.com>
2024-08-20 21:56:18 +0200

    WASD movement

---
+++ client/index.ts
@@ -0,0 +1,53 @@
+import Phaser from 'phaser';
+
+class Example extends Phaser.Scene
+{
+    keys: any
+    ship: Phaser.GameObjects.Image
+
+    preload ()
+    {
+        this.load.image('red', 'asset/ship.png');
+    }
+
+    create ()
+    {
+        this.ship = this.add.image(400, 300, 'red');
+        this.ship.displayWidth = 100;
+        this.ship.displayHeight = 100;
+
+        this.keys = this.input.keyboard.addKeys('W,A,S,D');
+    }
+
+    update() 
+    {
+        if(this.keys.W.isDown) {
+            this.ship.y -= 1;
+        }
+        if(this.keys.A.isDown) {
+            this.ship.x -= 1;
+        }
+        if(this.keys.S.isDown) {
+            this.ship.y += 1;
+        }
+        if(this.keys.D.isDown) {
+            this.ship.x += 1;
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    parent: "game",
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    }
+};
+
+const game = new Phaser.Game(config);
