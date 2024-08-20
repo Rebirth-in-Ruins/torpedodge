28779f748d219a9445c263209eb64fc351986bc6
Julien Midedji <Julien.Midedji@gmail.com>
2024-08-20 20:26:35 +0200

    Go adventures

--- server/main.go
+++ server/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello World!")
+
+	f := "apple"
+	// var ff string = "apple"
+	fmt.Println(f)
+
+	b, c := backtoback()
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+func backtoback() (int, int) {
+	return 1, 2
 }
